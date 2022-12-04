1f84e99ed864815d63609503b7a4c5d2d60e370a
Brady Barker <brady.d.barker@gmail.com>
2022-12-04 13:02:34 -0500

    Completed part_1.rb

---
+++ day_4/part_1.rb
@@ -0,0 +1,34 @@
+def format_assignment_times(input)
+  formatted_assignment_times = {}
+  input.each_with_index do |assignment, index|
+    pairs = assignment.split(",")
+    elf_1 = pairs[0].split("-")
+    elf_2 = pairs[1].split("-")
+    
+    formatted_assignment_times[index] = [elf_1, elf_2]
+  end
+  formatted_assignment_times
+end
+
+def compare_assignment_times(lower_1, upper_1, lower_2, upper_2)
+  return true if lower_1 <= lower_2 && upper_1 >= upper_2
+end
+
+def find_contained_pairs(assignment_times)
+  contained_pairs = 0
+  for assignments in assignment_times.values
+    lower_1 = assignments[0][0].to_i
+    upper_1 = assignments[0][1].to_i
+    lower_2 = assignments[1][0].to_i
+    upper_2 = assignments[1][1].to_i
+    
+    contained_pairs += 1 if compare_assignment_times(lower_1, upper_1, lower_2, upper_2) || compare_assignment_times(lower_2, upper_2, lower_1, upper_1)
+  end
+  contained_pairs
+end
+
+input = File.open('input.txt', 'r') {|file| file.readlines.join.split("\n")}
+
+assignment_times = format_assignment_times(input)
+
+p find_contained_pairs(assignment_times)
