14e612d7621e5c4ac1eb60e3dc1aefe56ef377f4
Brady Barker <brady.d.barker@gmail.com>
2022-12-09 21:51:34 -0500

    Completed day 8

--- day_8/part_2.rb
+++ day_8/part_2.rb
@@ -1,54 +1,63 @@
 def check_up(tree_input, tree_row_index, tree_column_index)
   row_index = tree_row_index
+  vision_score = 0
   while row_index > 0
     row_index -= 1
+    vision_score += 1
     if tree_input[tree_row_index][tree_column_index] <= tree_input[row_index][tree_column_index]
-      return false 
+      return vision_score  
     end
   end
-  true
+  vision_score
 end
 
 def check_down(tree_input, tree_row_index, tree_column_index)
   row_index = tree_row_index
+  vision_score = 0
   while row_index < tree_input[0].length-1
     row_index += 1
+    vision_score += 1
     if tree_input[tree_row_index][tree_column_index] <= tree_input[row_index][tree_column_index]
-      return false 
+      return vision_score 
     end
   end
-  true
+  vision_score
 end
 
 def check_left(tree_input, tree_row_index, tree_column_index)
   column_index = tree_column_index
+  vision_score = 0
   while column_index > 0
     column_index -= 1
+    vision_score += 1
     if tree_input[tree_row_index][tree_column_index] <= tree_input[tree_row_index][column_index]
-      return false 
+      return vision_score 
     end
   end
-  true
+  vision_score
 end
 
 def check_right(tree_input, tree_row_index, tree_column_index)
   column_index = tree_column_index
+  vision_score = 0
   while column_index < tree_input[0].length-1
     column_index += 1
+    vision_score += 1
     if tree_input[tree_row_index][tree_column_index] <= tree_input[tree_row_index][column_index]
-      return false
+      return vision_score
     end
   end
-  true
+  vision_score
 end
 
 tree_input = File.open('input.txt', 'r') {|file| file.readlines.join.split("\n")}
-total = 0
+scenic_score = []
 for tree_row_index in 0...tree_input.length
   for tree_column_index in 0...tree_input.length
-    total += 1 if check_up(tree_input, tree_row_index, tree_column_index) || check_down(tree_input, tree_row_index, tree_column_index) || check_left(tree_input, tree_row_index, tree_column_index) || check_right(tree_input, tree_row_index, tree_column_index)
+    #total += 1 if check_up(tree_input, tree_row_index, tree_column_index) || check_down(tree_input, tree_row_index, tree_column_index) || check_left(tree_input, tree_row_index, tree_column_index) || check_right(tree_input, tree_row_index, tree_column_index)
+    scenic_score.append(check_up(tree_input, tree_row_index, tree_column_index) * check_down(tree_input, tree_row_index, tree_column_index) * check_left(tree_input, tree_row_index, tree_column_index) * check_right(tree_input, tree_row_index, tree_column_index))
   end
 end
 
-p total
+p scenic_score.max
 
